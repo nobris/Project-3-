b0472f1cbeebf8a0afab8ba051071f7ee4934529
Tristan Clark <tristansclark@gmail.com>
2020-11-03 21:27:14 -0500

    Add files via upload

---
+++ Week 2 Deliverable.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+#Open log file
+f = open("userlog.log","r")
+
+#Return contents of log file
+lines = f.readlines()
+
+#Print the log file
+print(lines)
+
+
+# In[ ]:
+
+
+
+
